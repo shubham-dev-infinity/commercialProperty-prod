e2f3ba33fc90ae92b77d0834f5afc9c8de2edd84
vivekkumbhnai <8530vive@gmail.com>
2024-01-25 18:07:07 +0530

    add click event selecter

--- asserts/javascript/custom.js
+++ asserts/javascript/custom.js
@@ -24,8 +24,11 @@ $(document).ready(function() {
   });
 
   $(".custom-select-trigger").on("click", function (event) {
+      var isOpened = $(this).parents(".custom-select").hasClass("opened");
       $(".custom-select").removeClass("opened"); // Close all other selectors
-      $(this).parents(".custom-select").toggleClass("opened");
+      if (!isOpened) {
+          $(this).parents(".custom-select").addClass("opened");
+      }
       event.stopPropagation();
   });
 
@@ -41,6 +44,7 @@ $(document).ready(function() {
       $(".custom-select").removeClass("opened");
   });
 });
+
 
 
 //  ----------------------------------------------------------------------- GRAPH 1 ------------------------------------------------------------
